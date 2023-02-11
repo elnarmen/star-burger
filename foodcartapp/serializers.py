6aed2252cd11a2c857493b264071e059cf7c0724
Elnar Mengelbaev <elnar_men@mail.ru>
2023-02-11 15:31:48 +0300

    rewrite data validation logic with DRF.

---
+++ foodcartapp/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Order, OrderProduct
+
+
+class OrderProductSerializer(ModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(ModelSerializer):
+    products = OrderProductSerializer(many=True, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
