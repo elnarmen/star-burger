8a5f158f1e5fd2042c7e7c48c961d945115a5e29
Elnar Mengelbaev <elnar_men@mail.ru>
2023-05-06 15:36:24 +0300

    Добавил ip адрес

--- star-burger/star_burger/settings.py
+++ star-burger/star_burger/settings.py
@@ -16,7 +16,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '0.0.0.0'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '0.0.0.0', '167.71.43.140'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
