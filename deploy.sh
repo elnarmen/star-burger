b9c9bbea8e78dd5a9977383c5008f9b66459bb4e
Elnar Mengelbaev <elnar_men@mail.ru>
2023-05-06 15:40:19 +0300

    Исправил ошибки в названиях томов

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ git pull
 docker-compose -f docker-compose.prod.yml down
 docker-compose -f docker-compose.prod.yml up -d
 docker exec starburger_web python star-burger/manage.py migrate --no-input
-cp -r /var/lib/docker/volumes/starburger-docker_bundles_volume/_data/. /var/lib/docker/volumes/starburger-docker_static_volume/_data
-rm -f /var/lib/docker/volumes/starburger-docker_conf/_data/default.conf
-cp ./nginx.conf /var/lib/docker/volumes/starburger-docker_conf/_data
+cp -r /var/lib/docker/volumes/star-burger_bundles_volume/_data/. /var/lib/docker/volumes/star-burger_static_volume/_data
+rm -f /var/lib/docker/volumes/star-burger_conf/_data/default.conf
+cp ./nginx.conf /var/lib/docker/volumes/star-burger_conf/_data
 docker-compose -f docker-compose.prod.yml restart nginx
 
 echo "Deploy is done"
