be8f53fb002fd15043431b17e276c2ca30837561
elnar <elnar_men@mail.ru>
2023-05-07 21:00:24 +0000

    Настроил nginx на работу на домене elnar.tech

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ docker exec starburger_web python star-burger/manage.py migrate --no-input
 cp -r /var/lib/docker/volumes/star-burger_bundles_volume/_data/. /var/lib/docker/volumes/star-burger_static_volume/_data
 rm -f /var/lib/docker/volumes/star-burger_conf/_data/default.conf
 cp ./nginx.conf /var/lib/docker/volumes/star-burger_conf/_data
-docker exec starburger_nginx restart nginx
+docker-compose -f docker-compose.prod.yml restart nginx
+
 
 echo "Deploy is done"
