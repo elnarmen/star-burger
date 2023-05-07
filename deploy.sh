b600806fa79711f9b1ef96bdda553277a47ea258
Elnar Mengelbaev <elnar_men@mail.ru>
2023-05-07 14:13:08 +0300

    Изменил версию node

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ docker exec starburger_web python star-burger/manage.py migrate --no-input
 cp -r /var/lib/docker/volumes/star-burger_bundles_volume/_data/. /var/lib/docker/volumes/star-burger_static_volume/_data
 rm -f /var/lib/docker/volumes/star-burger_conf/_data/default.conf
 cp ./nginx.conf /var/lib/docker/volumes/star-burger_conf/_data
-docker-compose -f docker-compose.prod.yml restart nginx
+docker exec starburger_nginx restart nginx
 
 echo "Deploy is done"
